6ba932a0508fb90bb98479385ad2c1f3356dc4e9
Desmond <shoper2@163.com>
2025-08-02 14:27:38 +0800

    feat: add nonce to userInfo claims

--- lib/oidc_provider/account_to_user_info.rb
+++ lib/oidc_provider/account_to_user_info.rb
@@ -2,7 +2,7 @@ module OIDCProvider
   class AccountToUserInfo
     def call(account, scope_names, nonce)
       scopes = scope_names.map { |name| OIDCProvider.supported_scopes.detect { |scope| scope.name == name } }.compact
-      OpenIDConnect::ResponseObject::UserInfo.new(sub: account.send(OIDCProvider.account_identifier), nonce: nonce).tap do |user_info|
+      OpenIDConnect::ResponseObject::UserInfo.new({sub: account.send(OIDCProvider.account_identifier), nonce: nonce}).tap do |user_info|
         scopes.each do |scope|
           UserInfoBuilder.new(user_info, account).run(&scope.work)
         end
