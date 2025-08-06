38b6836d379e059694772a33436d3f62eac70a6c
Desmond <shoper2@163.com>
2025-08-06 17:09:29 +0800

    feat: include user claims in id token

--- app/models/oidc_provider/id_token.rb
+++ app/models/oidc_provider/id_token.rb
@@ -11,31 +11,27 @@ module OIDCProvider
     delegate :account, to: :authorization
 
     def to_response_object
+      base_claims ={
+        iss: OIDCProvider.issuer,
+        sub: account.send(OIDCProvider.account_identifier),
+        aud: authorization.client_id,
+        nonce: nonce,
+        exp: expires_at.to_i,
+        iat: created_at.to_i,
+        auth_time:created_at.to_i,
+        amr: ['pwd']
+      }
       if OIDCProvider.include_user_claims_in_id_token
-        OpenIDConnect::ResponseObject::IdTokenWithUserInfo.new(
-          iss: OIDCProvider.issuer,
-          sub: account.send(OIDCProvider.account_identifier),
-          aud: authorization.client_id,
-          nonce: nonce,
-          exp: expires_at.to_i,
-          iat: created_at.to_i,
-          auth_time:created_at.to_i,
-          amr: ['pwd'],
-          email: account.send(OIDCProvider.account_email),
-          email_verified: true,
-          given_name: account.send(OIDCProvider.account_given_name),
-          family_name: account.send(OIDCProvider.account_family_name),
-        )
+                   extra_claims = {
+                     email: account.send(OIDCProvider.account_email),
+                     email_verified: true,
+                     given_name: account.send(OIDCProvider.account_given_name),
+                     family_name: account.send(OIDCProvider.account_family_name),
+                   }
+        OpenIDConnect::ResponseObject::IdTokenWithUserInfo.new(**extra_claims, **base_claims)
       else
         OpenIDConnect::ResponseObject::IdToken.new(
-          iss: OIDCProvider.issuer,
-          sub: account.send(OIDCProvider.account_identifier),
-          aud: authorization.client_id,
-          nonce: nonce,
-          exp: expires_at.to_i,
-          iat: created_at.to_i,
-          auth_time:created_at.to_i,
-          amr: ['pwd']
+          **base_claims
         )
       end
     end
